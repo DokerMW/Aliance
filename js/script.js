600c801bd37bbca0c56778720221061dde88d433
DokerMW <abddoker@gmail.com>
2024-02-15 22:05:21 +0500

    add: blog-slider with adaptive + hover effect removed from mobile

--- js/script.js
+++ js/script.js
@@ -8,12 +8,12 @@ const menuBody = document.querySelector(".header__menu_mobile");
 const burgerLine = document.querySelectorAll(".burger-menu__icon span");
 
 const lightThemeOn = () => {
-  header.classList.add("dark");
-  burgerLine.forEach((el) => el.classList.add("dark"));
+   header.classList.add("dark");
+   burgerLine.forEach((el) => el.classList.add("dark"));
 };
 const lightThemeOff = () => {
-  header.classList.remove("dark");
-  burgerLine.forEach((el) => el.classList.remove("dark"));
+   header.classList.remove("dark");
+   burgerLine.forEach((el) => el.classList.remove("dark"));
 };
 const darkLogo = () => logo.setAttribute("href", "./img/icons/sprite.svg#main_logo_d");
 const lightLogo = () => logo.setAttribute("href", "./img/icons/sprite.svg#main_logo_l");
@@ -21,82 +21,103 @@ const minHeader = () => headerRow.classList.add("minify");
 const maxHeader = () => headerRow.classList.remove("minify");
 
 const changeHeaderTheme = () => {
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > 50) {
-      lightThemeOn();
-      minHeader();
-      darkLogo();
-    } else {
-      lightThemeOff();
-      maxHeader();
-      lightLogo();
-    }
-  });
+   window.addEventListener("scroll", function () {
+      if (window.scrollY > 50) {
+         lightThemeOn();
+         minHeader();
+         darkLogo();
+      } else {
+         lightThemeOff();
+         maxHeader();
+         lightLogo();
+      }
+   });
 };
 
 const mMenuToggle = () => {
-  burgerButton.addEventListener("click", function (e) {
-    document.body.classList.toggle("_lock");
-    iconMenu.classList.toggle("_active");
-    menuBody.classList.toggle("_active");
-    if (body.classList.contains("_lock")) {
-      darkLogo();
-    } else if (!body.classList.contains("_lock") && window.scrollY < 50) {
-      lightLogo();
-    }
-    if (window.scrollY < 50) {
-      burgerLine.forEach((el) => el.classList.toggle("dark"));
-    }
-  });
+   burgerButton.addEventListener("click", function (e) {
+      document.body.classList.toggle("_lock");
+      iconMenu.classList.toggle("_active");
+      menuBody.classList.toggle("_active");
+      if (body.classList.contains("_lock")) {
+         darkLogo();
+      } else if (!body.classList.contains("_lock") && window.scrollY < 50) {
+         lightLogo();
+      }
+      if (window.scrollY < 50) {
+         burgerLine.forEach((el) => el.classList.toggle("dark"));
+      }
+   });
 };
 
 changeHeaderTheme();
 mMenuToggle();
 
 const swiperFeatures = new Swiper(".features-slider", {
-  direction: "horizontal",
-  loop: false,
-  navigation: {
-    nextEl: ".features-next-btn",
-    prevEl: ".features-prev-btn",
-  },
-  slidesPerView: 3.5,
-  breakpoints: {
-    320: {
-      slidesPerView: 1.38,
-    },
-    550: {
-      slidesPerView: 2.4,
-    },
-    768: {
-      slidesPerView: 3.4,
-    },
-    900: {
-      slidesPerView: 4.4,
-    },
-  },
+   direction: "horizontal",
+   loop: false,
+   navigation: {
+      nextEl: ".features-next-btn",
+      prevEl: ".features-prev-btn",
+   },
+   slidesPerView: 3.5,
+   breakpoints: {
+      320: {
+         slidesPerView: 1.38,
+      },
+      550: {
+         slidesPerView: 2.4,
+      },
+      768: {
+         slidesPerView: 3.4,
+      },
+      900: {
+         slidesPerView: 4.4,
+      },
+   },
 });
 
 const swiperScheme = new Swiper(".scheme-slider", {
-  direction: "horizontal",
-  loop: false,
-  navigation: {
-    nextEl: ".scheme-next-btn",
-    prevEl: ".scheme-prev-btn",
-  },
-  slidesPerView: 3.5,
-  breakpoints: {
-    320: {
-      slidesPerView: 1.1,
-    },
-    550: {
-      slidesPerView: 2.4,
-    },
-    768: {
-      slidesPerView: 3.4,
-    },
-    900: {
-      slidesPerView: 4.4,
-    },
-  },
+   direction: "horizontal",
+   loop: false,
+   navigation: {
+      nextEl: ".scheme-next-btn",
+      prevEl: ".scheme-prev-btn",
+   },
+   slidesPerView: 3.5,
+   breakpoints: {
+      320: {
+         slidesPerView: 1.1,
+      },
+      550: {
+         slidesPerView: 2.4,
+      },
+      768: {
+         slidesPerView: 3.4,
+      },
+      900: {
+         slidesPerView: 4.4,
+      },
+   },
+});
+
+const swiperBlog = new Swiper(".swiper-blog", {
+   direction: "horizontal",
+   loop: false,
+   navigation: {
+      nextEl: ".swiper-blog__btn_n",
+      prevEl: ".swiper-blog__btn_p",
+   },
+   slidesPerView: 2,
+   breakpoints: {
+      320: {
+         slidesPerView: 1,
+      },
+      900: {
+         slidesPerView: 1.3,
+      },
+      1400: {
+         slidesPerView: 2,
+      },
+   },
 });
