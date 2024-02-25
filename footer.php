91be9f163e8af39c4106e3c420d2645a5636af7c
DokerMW <abddoker@gmail.com>
2024-02-25 20:18:37 +0500

    add: footer.php

---
+++ footer.php
@@ -0,0 +1,79 @@
+<footer class="footer">
+            <div class="footer__top top-footer">
+               <div class="top-footer__container _container">
+                  <div class="top-footer__logo">
+                     <svg class="footer__img">
+                        <use href="./img/icons/sprite.svg#main_logo_d"></use>
+                     </svg>
+                  </div>
+                  <a href="tel:+74996861014" class="top-footer__tel">+7 (555) 686-10-14</a>
+                  <div class="top-footer__info">
+                     <div class="top-footer__address">г. Мосвка, Холодильный пер. 4к1с8</div>
+                     <a href="mailto:a.dragunov@tdaliance.ru" class="top-footer__email"
+                        >a.dragunov@tdaliance.ru</a
+                     >
+                  </div>
+                  <div class="top-footer__social">
+                     <img src="img/icons/vk.svg" alt="" />
+                     <img src="img/icons/inst.svg" alt="" />
+                  </div>
+               </div>
+            </div>
+            <nav class="footer__middle middle-footer">
+               <div class="middle-footer__container _container">
+                  <div class="middle-footer__column">
+                     <h2 class="middle-footer__title"><a href="#">Контрактное производство</a></h2>
+                     <ul class="middle-footer_list">
+                        <li class="middle-footer__item">
+                           <a href="#">Автомобильная химия</a>
+                        </li>
+                        <li class="middle-footer__item"><a href="#">Бытовая химия</a></li>
+                        <li class="middle-footer__item">
+                           <a href="#">Дезинфицирующие средства</a>
+                        </li>
+                        <li class="middle-footer__item"><a href="#">Пищевые аэрозоли</a></li>
+                        <li class="middle-footer__item">
+                           <a href="#">Косметическая продукция</a>
+                        </li>
+                        <li class="middle-footer__item">
+                           <a href="#">Краски аэрозольные</a>
+                        </li>
+                     </ul>
+                  </div>
+                  <div class="middle-footer__column">
+                     <h2 class="middle-footer__title">
+                        <a href="#">Собственные марки</a>
+                     </h2>
+                     <ul class="middle-footer_list">
+                        <li class="middle-footer__item">
+                           <a href="#">Автохимия AG-Tech</a>
+                        </li>
+                        <li class="middle-footer__item"><a href="#">Автохимия AP</a></li>
+                     </ul>
+                  </div>
+                  <div class="middle-footer__column">
+                     <ul class="middle-footer_list">
+                        <li class="middle-footer__title"><a href="#">О компании</a></li>
+                        <li class="middle-footer__title"><a href="#">Новости</a></li>
+                        <li class="middle-footer__title"><a href="#">Контакты</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </nav>
+            <div class="footer__bottom bottom-footer">
+               <div class="bottom-footer__container _container">
+                  <div class="bottom-footer__row">
+                     <div class="bottom-footer__item">
+                        <span>© <?php echo date('Y') ?> «Aliance Production». Все права защищены.</span>
+                        <a href="#">Политики конфиденциальности</a>
+                     </div>
+                     <div class="bottom-footer__item">
+                        <span>Сделано в</span>
+                        <svg class="bottom-footer__icon">
+                           <use href="./img/icons/sprite.svg#ruso"></use>
+                        </svg>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </footer>
