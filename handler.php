aac0572d2a8c95870e23ae0a5eff2e36672b9c69
DokerMW <abddoker@gmail.com>
2024-02-26 20:29:44 +0500

    add: send message in tg-bot

--- handler.php
+++ handler.php
@@ -1,5 +1,18 @@
 <?php 
-$user_name = $_POST['username'];
-$user_tel = $_POST['usertel'];
+$user_name = htmlspecialchars($_POST['username']);
+$user_tel = htmlspecialchars($_POST['usertel']);
 
+$token = "7014377967:AAHkC7zMPbGnL8Vj8Q4yrXnatuBme_GirPI";
+$chat_id = "-4121775222";
+
+$formData = array(
+	"Клиент: " => $user_name,
+	"Телефон: " => $user_tel
+);
+
+foreach($formData as $key => $value) {
+	$text .= $key . urlencode($value) . "%0A" ;
+}
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}", "r");
 ?>
