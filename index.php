b91b5c894ad69958d84bf3f286d634f94fa70e24
DokerMW <abddoker@gmail.com>
2024-02-27 10:26:34 +0500

    add: include validation js

--- index.php
+++ index.php
@@ -647,6 +647,7 @@
          <?php include_once('./footer.php') ?>
       </div>
       <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+			<script src="./js/just-validate.production.min.js"></script>
       <script src="./js/script.js"></script>
    </body>
 </html>
