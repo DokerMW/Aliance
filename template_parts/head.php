c29212b3641a55f329936b2f98ee3d412b341b38
DokerMW <abddoker@gmail.com>
2024-03-04 20:39:37 +0500

    fix: swiper-bundle css in local files

--- template_parts/head.php
+++ template_parts/head.php
@@ -3,7 +3,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="format-detection" content="telephone=no" />
 	<link rel="shortcut icon" href="./img/icons/favicon.ico" type="image/x-icon" />
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+	<link rel="stylesheet" href="./css/swiper-bundle.min.css" />
 	<link rel="stylesheet" href="./css/style.css" />
 	<?php echo $extra_styles ?>
 	<script src="./js/swiper-bundle.min.js" defer></script>
